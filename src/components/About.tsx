f80d44e78515079c59a245a7f46d8dc097e87016
Sanberk Türker <69405619+sanberkHax@users.noreply.github.com>
2023-03-31 21:42:37 +0300

    Add about section

---
+++ src/components/About.tsx
@@ -0,0 +1,25 @@
+import { Typography } from './Typography';
+
+export const About = () => {
+  return (
+    <section className="flex justify-center gap-10 items-center flex-col h-screen  w-full text-white">
+      <Typography variant="h2" bold>
+        About Me
+      </Typography>
+      <div className="border-4 border-cyan-300 rounded-md w-1/2 p-6 indent-10">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit,
+        omnis modi quibusdam illo nulla aspernatur repudiandae mollitia iste
+        consectetur explicabo iusto ut, nam porro incidunt facere error sequi,
+        repellendus fugit vel eligendi est impedit deleniti? Repudiandae esse
+        tempore modi magnam consequuntur perspiciatis, quibusdam, nesciunt
+        tempora perferendis maiores doloremque saepe, fugiat error
+        exercitationem autem accusantium debitis dolorum aliquam. Molestiae
+        maiores culpa totam itaque amet recusandae maxime ut vel officiis
+        corrupti consectetur, quasi eius minus animi error in vitae nemo ipsa
+        cumque perspiciatis nobis blanditiis quam laudantium aut! Enim corrupti
+        reiciendis illo, earum praesentium, beatae quibusdam eveniet recusandae
+        expedita debitis modi sunt.
+      </div>
+    </section>
+  );
+};
