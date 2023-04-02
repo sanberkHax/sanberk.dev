339c199b384ecfd693718f8b93fb3e9b7ef4eff4
Sanberk Türker <69405619+sanberkHax@users.noreply.github.com>
2023-04-03 01:02:39 +0300

    Update favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,6 +15,23 @@ export default function Home() {
         <meta name="description" content="Personal website of Sanberk Türker" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
       </Head>
       <Header />
       <main className="bg-gradient-to-b from-[#005687] to-[#00112C] min-h-screen flex flex-col justify-center w-full py-2 px-8 sm:px-12 md:px-18 lg:px-24 xl:px-36  2xl:px-64 gap-96 items-center">
--- src/pages/real-store.tsx
+++ src/pages/real-store.tsx
@@ -15,6 +15,23 @@ export default function RealStore() {
         <meta name="description" content="Stack Organizer" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
       </Head>
       <header className="absolute top-10 left-8 z-10">
         <Button variant="sm" link="/">
