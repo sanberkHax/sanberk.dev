5371303945c01bb5dfafb6cb5eb94eac9dc639f0
Sanberk Türker <69405619+sanberkHax@users.noreply.github.com>
2023-04-02 17:21:03 +0300

    Add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,21 @@
+import { Icon } from './Icon';
+import { Typography } from './Typography';
+
+export const Footer = () => {
+  return (
+    <footer className="p-4 flex-col lg:px-10 lg:flex-row bg-cyan-300 flex justify-between items-center gap-4">
+      <Typography variant="body" className="">
+        Designed & Built by <b>Sanberk Türker</b>
+      </Typography>
+      <div className="flex gap-10">
+        <Icon name="GitHub" link="https://github.com/sanberkHax" />
+        <Icon
+          name="Linkedin"
+          link="https://www.linkedin.com/in/sanberk-turker/"
+        />
+        <Icon name="Twitter" link="https://twitter.com/sanberkDev" />
+        <Icon name="Gmail" link="mailto:sanberk.trker@gmail.com" />
+      </div>
+    </footer>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { About } from '@/components/About';
 import { Contact } from '@/components/Contact';
+import { Footer } from '@/components/Footer';
 import { Hero } from '@/components/Hero';
 import { ProjectList } from '@/components/ProjectList';
 import { Technologies } from '@/components/Technologies';
@@ -14,13 +15,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-gradient-to-b from-[#005687] to-[#00112C] min-h-screen flex flex-col justify-center w-full py-2 px-8 sm:px-12 md:px-18 lg:px-24 xl:px-36 2xl:px-48 gap-96 items-center">
+      <main className="bg-gradient-to-b from-[#005687] to-[#00112C] min-h-screen flex flex-col justify-center w-full py-2 px-8 sm:px-12 md:px-18 lg:px-24 xl:px-36  2xl:px-64 gap-96 items-center">
         <Hero />
         <About />
         <Technologies />
         <ProjectList />
         <Contact />
       </main>
+      <Footer />
     </>
   );
 }
